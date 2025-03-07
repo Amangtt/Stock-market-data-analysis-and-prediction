72c2aace8cd8896d77638d43b0a922d86e237d6f
Amangtt <amangtebeje123@gmail.com>
2025-03-07 09:33:54 +0300

    edited

--- scripts/Sentiment_analysis.py
+++ scripts/Sentiment_analysis.py
@@ -16,43 +16,52 @@ sid = SentimentIntensityAnalyzer()
 
 lemmatizer = WordNetLemmatizer()
 stop_words = set(stopwords.words('english'))
-df=pd.read_csv('./Data/raw_analyst_ratings.csv')
+
 class sentiment:
-        def __init__(self,data):
+        def __init__(self,data,logger=None):
                 self.df=data
-
+                self.logger=logger
         def preprocess(self,text):
-                # Tokenize
-                tokens = word_tokenize(text.lower())
-                # Remove punctuation and stopwords
-                tokens = [word for word in tokens if word.isalnum() and word not in stop_words]
-                # Lemmatize
-                tokens = [lemmatizer.lemmatize(word) for word in tokens]
-                return " ".join(tokens)  # Convert tokens back to a string
-
+                try:
+                        # Tokenize
+                        tokens = word_tokenize(text.lower())
+                        # Remove punctuation and stopwords
+                        tokens = [word for word in tokens if word.isalnum() and word not in stop_words]
+                        # Lemmatize
+                        tokens = [lemmatizer.lemmatize(word) for word in tokens]
+                        self.logger.info("Word tokenization completed successfully.")
+                        return " ".join(tokens)  # Convert tokens back to a string
+                
+                except Exception as e:
+                        error_message = f"Failed to tokenize words: {e}"
+                        self.logger.error(error_message)
 
         def sentiment_analysis(self):
         # Apply preprocessing
-                df=self.df
-                df['processed_headline'] = df['headline'].apply(self.preprocess)
+                try:
+                        df=self.df
+                        df['processed_headline'] = df['headline'].apply(self.preprocess)
 
-                # Select first 50 rows as a DataFrame
-                head = df[['headline', 'processed_headline']].head(50).copy()
+                        # Select first 50 rows as a DataFrame
+                        head = df[['headline', 'processed_headline']].head(50).copy()
 
-                # Sentiment Analysis using TextBlob
-                head['score'] = head['processed_headline'].apply(lambda x: TextBlob(x).sentiment.polarity)
+                        # Sentiment Analysis using TextBlob
+                        head['score'] = head['processed_headline'].apply(lambda x: TextBlob(x).sentiment.polarity)
 
-                # Load spaCy model for Topic Modeling
-                nlp = spacy.load("en_core_web_sm")
-                head['keywords'] = head['processed_headline'].apply(lambda x: [token.text for token in nlp(x) if token.pos_ in ["NOUN", "PROPN"]])
+                        # Load spaCy model for Topic Modeling
+                        nlp = spacy.load("en_core_web_sm")
+                        head['keywords'] = head['processed_headline'].apply(lambda x: [token.text for token in nlp(x) if token.pos_ in ["NOUN", "PROPN"]])
 
-                # Assign sentiment labels
-                head['sentiment'] = head['score'].apply(lambda score: 'Positive' if score >= 0.05 
-                                                        else ('Negative' if score <= -0.05 else 'Neutral'))
-        
-                # Print output
-                return head
-c=sentiment(df)
-c.sentiment_analysis()
+                        # Assign sentiment labels
+                        head['sentiment'] = head['score'].apply(lambda score: 'Positive' if score >= 0.05 
+                                                                else ('Negative' if score <= -0.05 else 'Neutral'))
+                        self.logger.info("Sentiment analysis completed successfully.")
+                        # Print output
+                        return head
+                except Exception as e:
+                        error_message = f"Sentiment analysis falied: {e}"
+                        self.logger.error(error_message)        
+                
+
 
 
--- scripts/eda.py
+++ scripts/eda.py
@@ -4,43 +4,55 @@ from datetime import datetime
 
 
 class Descriptive:
-    def __init__(self, data):
+    def __init__(self, data, logger=None):
         self.df=data
-
+        self.logger=logger
 
     def headline_length(self):
-        self.df["headline length"] = self.df["headline"].apply(len)
-        return self.df
- 
+        try:
+            self.df["headline length"] = self.df["headline"].apply(len)
+
+            self.logger.info("headline length calculated successfully.")
+            return self.df
+        except Exception as e:
+            error_message = f"Failed to calculate length for: {e}"
+            self.logger.error(error_message)
     def check_missing(self):
-        
+        self.logger.info("missing values calculated successfully.")
         return self.df.isnull().sum()
 
 
-    def no_article(self):
-        publisher=self.df.groupby('publisher').size().reset_index(name='publisher count')
-        publisher=publisher.sort_values(by='publisher count', ascending=False)
-        print(publisher)
-        
+ 
     def stock(self):
-        df=self.df
-        df['date'] = df['date'].str[:10]
-        df['date']= pd.to_datetime(df['date'])
-        stocks=df.groupby('stock').size().reset_index(name='stock count')
-        stocks=stocks.sort_values(by='stock count', ascending=False)
-        return stocks.head(5)
-    
+        try:
+            df=self.df
+            df['date'] = df['date'].str[:10]
+            df['date']= pd.to_datetime(df['date'])
+            stocks=df.groupby('stock').size().reset_index(name='stock count')
+            stocks=stocks.sort_values(by='stock count', ascending=False)
+            top_5=stocks.head(5)
+            self.logger.info("Number of stocks with the most number of articles calculated successfully.")
+            return top_5
+        except Exception as e:
+            error_message = f"Failed to calculate Stock count {e}"
+            self.logger.error(error_message)
+
     def plot_article_over_time(self):
-        df=self.df
-        df['date'] = df['date'].str[:10]
-        df['date'] = pd.to_datetime(df['date'])
-        count_by_date= df.groupby('date').size().reset_index(name='Trend by date')
-        plt.figure(figsize=(12, 6))
-        plt.plot(count_by_date['date'], count_by_date['Trend by date'], marker='o', linestyle='-', color='skyblue')
-        plt.title('Number of Articles Published Over Time')
-        plt.xlabel('Date')
-        plt.ylabel('Number of Articles')
-        plt.xticks(rotation=45)
-        plt.grid(axis='y')
-        plt.tight_layout()
-        plt.show()
+        try:
+            df=self.df
+            df['date'] = df['date'].str[:10]
+            df['date'] = pd.to_datetime(df['date'])
+            count_by_date= df.groupby('date').size().reset_index(name='Trend by date')
+            self.logger.info("Ploting articles published overtime.")
+            plt.figure(figsize=(12, 6))
+            plt.plot(count_by_date['date'], count_by_date['Trend by date'], marker='o', linestyle='-', color='skyblue')
+            plt.title('Number of Articles Published Over Time')
+            plt.xlabel('Date')
+            plt.ylabel('Number of Articles')
+            plt.xticks(rotation=45)
+            plt.grid(axis='y')
+            plt.tight_layout()
+            plt.show()
+        except Exception as e:
+            error_message = f"Failed to plot article overtime {e}"
+            self.logger.error(error_message)
